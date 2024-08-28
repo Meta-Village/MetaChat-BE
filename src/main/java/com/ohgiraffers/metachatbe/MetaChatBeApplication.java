d50a4fb7bd7d1162dde1d958cc723ea4cc2bcf6e
GarlGaru <juon304@gmail.com>
2024-08-28 16:17:00 +0900

    initialize

---
+++ src/main/java/com/ohgiraffers/metachatbe/MetaChatBeApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.metachatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaChatBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaChatBeApplication.class, args);
+	}
+
+}
