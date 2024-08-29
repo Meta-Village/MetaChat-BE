a58f1a7f8420f5777a1dd3a0dc74586c63900749
jeonjinhyun <jjh06500@gmail.com>
2024-08-29 11:04:15 +0900

    "spring security"

--- src/main/java/com/ohgiraffers/metachatbe/security/common/utils/TokenUtils.java
+++ src/main/java/com/ohgiraffers/metachatbe/security/common/utils/TokenUtils.java
@@ -1,5 +1,5 @@
 package com.ohgiraffers.metachatbe.security.common.utils;
-
+import io.jsonwebtoken.ExpiredJwtException;
 import com.ohgiraffers.metachatbe.user.entity.User;
 import io.jsonwebtoken.*;
 import jakarta.xml.bind.DatatypeConverter;
