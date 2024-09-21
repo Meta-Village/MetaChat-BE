1c72b63ad69ff6841c2169de0a1738daede9c903
GARLGARU\GarlGaru <juon304@gmail.com>
2024-09-22 01:26:40 +0900

    스웨거 추가, 쿼리 파라미터 변수명 수정

--- src/main/java/com/ohgiraffers/metachatbe/summary/command/application/controller/SummaryWebController.java
+++ src/main/java/com/ohgiraffers/metachatbe/summary/command/application/controller/SummaryWebController.java
@@ -5,6 +5,10 @@ import com.ohgiraffers.metachatbe.meeting.command.application.service.MeetingSer
 import com.ohgiraffers.metachatbe.summary.command.application.dto.SummaryDTO;
 import com.ohgiraffers.metachatbe.summary.command.domain.service.SummaryDomainService;
 import com.ohgiraffers.metachatbe.util.enumtype.ZoneName;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+
 @Controller
+@Tag(name = "요약 웹페이지", description = "요약 웹페이지 구조")
 public class SummaryWebController {
 
 
@@ -27,22 +33,35 @@ public class SummaryWebController {
     }
 
 
+    @Operation(summary = "요약 검색", description = "월드와 존 넘버 기준으로 검색합니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "페이지를 불러옵니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
+    })
     // Form 페이지 로드
     @GetMapping("/web-summary/search")
     public String searchSummaryForm() {
         return "summary/find-summary";
     }
 
-    // Form 처리 및 결과 페이지로 이동
+    @Operation(summary = "검색 결과 표시", description = "해당 지역에서 발생한 모든 회의를 불러옵니다.<br/><br/>" +
+            "검색된 회의 결과가 하나 뿐인 경우, 요약 내용 표시 페이지로 리다이렉트 됩니다.<br/><br/>" +
+            "쿼리 파라미터로 바로 검색이 가능합니다.<br/>쿼리 파라미터를 이용할 경우 zone-name 설정에 주의하세요.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "페이지를 불러옵니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
+    })
     @GetMapping("/web-summary/summary-list")
-    public String findSummaryList(@RequestParam("world_id") long worldId,
-                                  @RequestParam("zone_name") String zoneName,
+    public String findSummaryList(@RequestParam("world-id") long worldId,
+                                  @RequestParam("zone-name") String zoneName,
                                   Model model) {
         System.out.println("검색을 시작합니다 : " + worldId + " / " + zoneName);
 
         if (isNOTValidZoneName(zoneName)) {
             model.addAttribute("error",
                     "zone_name 이 유효하지 않습니다 : " + zoneName);
+            model.addAttribute("errorCode",
+                    HttpStatus.BAD_REQUEST);
             return "errorPage";
         }
 
@@ -51,18 +70,20 @@ public class SummaryWebController {
 
         if (meetings == null || meetings.isEmpty()) {
             model.addAttribute("error",
-                    "No meetings found for world_id: " + worldId + " and zone_name: " + zoneName);
+                    "해당하는 회의를 찾을 수 없습니다  world-id: " + worldId + " / zone-name: " + zoneName);
+            model.addAttribute("errorCode",
+                    HttpStatus.BAD_REQUEST);
             return "errorPage";
         }
 
         if (meetings.size() == 1) {
-            return "redirect:/web-summary/summary?meetingId=" + meetings.getFirst().getMeetingId();
+            return "redirect:/web-summary/summary?meeting-id=" + meetings.getFirst().getMeetingId();
         }
 
         //결과 출력
         System.out.println(meetings);
         model.addAttribute("meetings", meetings);
-        return "summary/summaryList";
+        return "summary/summary-list";
     }
 
     private boolean isNOTValidZoneName(String zoneName) {
@@ -80,8 +101,16 @@ public class SummaryWebController {
         }
     }
 
+
+    @Operation(summary = "요약 내용 표시", description = "검색된 미팅 ID로 요약을 불러옵니다<br/>" +
+            "미팅 ID를 알고 있다면 쿼리 파라미터로 바로 입력해서 열람할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "페이지를 불러옵니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
+            @ApiResponse(responseCode = "500", description = "서버 내부 에러")
+    })
     @GetMapping("/web-summary/summary")
-    public String findSummary(@RequestParam long meetingId, Model model) {
+    public String findSummary(@RequestParam("meeting-id") long meetingId, Model model) {
         System.out.println("요약을 가져옵니다. : " + meetingId);
 
         MeetingDTO meeting = meetingService.findMeetingById(meetingId);
